2e89502e95d842041acbbc2e8bd8aae1701d8906
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-03-28 01:08:04 -0300

    feat <VIS-23>: Add swagger api
    - Add swagger documentation for UserController
    - Add swagger documentation for AreaController

--- src/main/java/coderhood/controller/AreaController.java
+++ src/main/java/coderhood/controller/AreaController.java
@@ -3,6 +3,11 @@ package coderhood.controller;
 import coderhood.dto.AreaDto;
 import coderhood.model.Area;
 import coderhood.service.AreaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,30 +23,61 @@ public class AreaController {
     @Autowired
     private AreaService areaService;
 
+    @Operation(summary = "Criar área")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Área criada com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class))),
+        @ApiResponse(responseCode = "400", description = "Entrada inválida", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<Area> createArea(@Valid @RequestBody AreaDto areaDTO) {
         Area area = areaService.createArea(areaDTO);
         return new ResponseEntity<>(area, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Consultar área por ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Área localizada com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class)))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Area>> getAreaById(@PathVariable UUID id) {
         Optional<Area> area = areaService.findAreaById(id);
         return ResponseEntity.ok(area);
     }
 
+    @Operation(summary = "Consultar todas as áreas")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Áreas localizadas com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class)))
+    })
     @GetMapping
     public ResponseEntity<Iterable<Area>> getAllAreas() {
         Iterable<Area> areas = areaService.findAllAreas();
         return ResponseEntity.ok(areas);
     }
 
+    @Operation(summary = "Editar área por ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Área editada com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class)))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Area> updateArea(@PathVariable UUID id, @Valid @RequestBody AreaDto areaDto) {
         Area updatedArea = areaService.updateArea(id, areaDto);
         return ResponseEntity.ok(updatedArea);
     }
 
+    @Operation(summary = "Deletar área por ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Área deletada com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteArea(@PathVariable UUID id) {
         areaService.deleteArea(id);
