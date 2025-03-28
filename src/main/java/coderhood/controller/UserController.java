f2883c1425d66182880de2fca9ffd05c8c49e11d
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-03-28 01:08:04 -0300

    feat <VIS-23>: Add swagger api
    - Add swagger documentation for UserController
    - Add swagger documentation for AreaController

--- src/main/java/coderhood/controller/UserController.java
+++ src/main/java/coderhood/controller/UserController.java
@@ -2,7 +2,13 @@ package coderhood.controller;
 
 import coderhood.dto.UserRequestDto;
 import coderhood.dto.UserResponseDto;
+import coderhood.model.Area;
 import coderhood.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 import java.util.UUID;
@@ -27,12 +33,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Criar usuário")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Usuário criado com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class))),
+        @ApiResponse(responseCode = "400", description = "Entrada inválida", content = @Content)
+    })
     @PostMapping("/users")
     public ResponseEntity<UserResponseDto> userCreate(@Valid @RequestBody UserRequestDto userRequestDto) {
         UserResponseDto userResponseDTO = userService.userCreate(userRequestDto);
         return new ResponseEntity<>(userResponseDTO, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Editar usuário por ID")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Usuário editado com sucesso", 
+                     content = @Content(mediaType = "application/json", 
+                     schema = @Schema(implementation = Area.class)))
+    })
     @PutMapping("user/{id}")
     public String userUpdate(@PathVariable UUID id, @RequestBody UserRequestDto userRequestDto) {
         return userService.userUpdate(id, userRequestDto);
