85c479547ad4d907bf6bfa8a8ab2acf8bbd10c67
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2025-03-27 21:27:51 -0300

    fix <VIS-23> Adicionando cabeçalho

--- src/main/java/coderhood/controller/AreaController.java
+++ src/main/java/coderhood/controller/AreaController.java
@@ -5,9 +5,11 @@ import coderhood.model.Area;
 import coderhood.service.AreaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,26 +23,45 @@ public class AreaController {
 
     @PostMapping
     public ResponseEntity<Area> createArea(@Valid @RequestBody AreaDto areaDTO) {
-        Area area = areaService.createArea(areaDTO);
-        return new ResponseEntity<>(area, HttpStatus.CREATED);
+        try {
+            Area area = areaService.createArea(areaDTO);
+
+            // Criando cabeçalho para garantir que a resposta seja JSON
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+
+            return ResponseEntity.status(HttpStatus.CREATED).headers(headers).body(area);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(null);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Area>> getAreaById(@PathVariable UUID id) {
         Optional<Area> area = areaService.findAreaById(id);
-        return ResponseEntity.ok(area);
+
+        if (area.isPresent()) {
+            return ResponseEntity.ok().header("Content-Type", "application/json").body(area);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @GetMapping
     public ResponseEntity<Iterable<Area>> getAllAreas() {
         Iterable<Area> areas = areaService.findAllAreas();
-        return ResponseEntity.ok(areas);
+        return ResponseEntity.ok().header("Content-Type", "application/json").body(areas);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Area> updateArea(@PathVariable UUID id, @Valid @RequestBody AreaDto areaDto) {
-        Area updatedArea = areaService.updateArea(id, areaDto);
-        return ResponseEntity.ok(updatedArea);
+        try {
+            Area updatedArea = areaService.updateArea(id, areaDto);
+            return ResponseEntity.ok().header("Content-Type", "application/json").body(updatedArea);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
     @DeleteMapping("/{id}")
