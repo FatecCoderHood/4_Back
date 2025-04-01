7259fbc6d2d4610264535200b7228b88cbbdf730
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-04-01 16:27:18 -0300

    chore <VIS-23>: Add user auth flow

--- src/main/java/coderhood/model/User.java
+++ src/main/java/coderhood/model/User.java
@@ -50,4 +50,50 @@ public class User {
                 .collect(Collectors.toList());
         }
     }
+
+    public enum RoleEnum {
+        ADMINISTRATOR(TipoAcesso.ADMIN, "Administrador"),
+        CONSULTANT(TipoAcesso.CONSULTOR, "Consultor"),
+        ANALYST(TipoAcesso.ANALISTA, "Analista");
+    
+        private final TipoAcesso code;
+        private final String description;
+    
+        // Constructor
+        RoleEnum(TipoAcesso code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+    
+        // Getter methods
+        public TipoAcesso getCode() {
+            return code;
+        }
+    
+        public String getDescription() {
+            return description;
+        }
+    
+        // Method to map role code to description
+        public static String getRoleDescriptionByCode(TipoAcesso code) {
+            for (RoleEnum role : RoleEnum.values()) {
+                if (role.getCode() == code) {
+                    return role.getDescription();
+                }
+            }
+            return null; // or you can return a default value
+        }
+    }
+
+    public String getName() {
+        return this.nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getRole() {
+        return RoleEnum.getRoleDescriptionByCode(this.tipoAcesso); // Mapping code to string
+    }
 }
